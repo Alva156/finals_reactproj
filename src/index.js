4458ec95c9b4f0fcea3df456e53c32729fb0b509
Cid Alvarico <andreicimoune.alvarico.cics@ust.edu.ph>
2024-05-13 23:07:15 +0800

    initial

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ root.render(
     <BookHispage />
   </React.StrictMode>
 );
+// rawr
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
