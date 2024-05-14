f5da88b1264462a4d8167c11346a5dff946f9443
Cid Alvarico <andreicimoune.alvarico.cics@ust.edu.ph>
2024-05-14 22:18:17 +0800

    improvement 10:18pm

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Login />
+    <Checkoutedit />
   </React.StrictMode>
 );
 
