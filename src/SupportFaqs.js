5b53ce164f0a351a94f8355b176218b092fd7753
Qiroo <valdecanasjustin@gmail.com>
2024-05-16 12:25:05 +0800

    kk

--- src/SupportFaqs.js
+++ src/SupportFaqs.js
@@ -21,7 +21,7 @@ const SupportFaqs = () => {
     "Booking your trip with SeoulSeeker is simple! Just navigate to our homepage, select your desired destination, dates, and preferences, and browse through the available options. Once you find the perfect package, click Book Now and follow the prompts to complete your reservation securely.",
     "At SeoulSeeker, your safety is our top priority. We adhere to all local and international health guidelines to ensure a safe and enjoyable travel experience. This includes regular sanitization of accommodations, transportation, and adherence to social distancing protocols. Please check our COVID-19 information page for the latest updates and requirements.",
     "Absolutely! SeoulSeeker offers customizable travel packages tailored to your preferences and interests. Whether you're looking for a cultural tour, adventure activities, or a relaxing getaway, our team can create a personalized itinerary just for you. Simply contact our customer support team to discuss your options.",
-    "We accept various payment methods for your convenience, including credit/debit cards, PayPal, and bank transfers. Rest assured, all transactions made on SeoulSeeker are secure and encrypted to protect your sensitive information.",
+    "We only accept onsite payments. Rest assured, all transactions made on SeoulSeeker are secure and encrypted to protect your sensitive information.",
     "SeoulSeeker provides 24/7 customer support to assist you throughout your journey. If you encounter any issues or require assistance during your trip, simply reach out to our dedicated support team via phone, email, or live chat. We're here to ensure your travel experience is seamless and enjoyable from start to finish.",
   ];
 
